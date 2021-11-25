fc03004c6b2b92565913d7398d6c4704725bc3ed
Thomas.D <thomlgt@hotmail.fr>
2021-11-25 13:22:08 +0100

    captain model

---
+++ back/src/models/captain.ts
@@ -0,0 +1,10 @@
+export class Captain {
+    id : string;
+    firstname : string;
+    lastname : string;
+    email : string;
+    password : string;
+    phone : string;
+    birthdate : Date;
+    gender : boolean;
+}
