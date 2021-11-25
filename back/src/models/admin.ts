31495209bb0b86a1ab598f594ab8b233e88b5046
Thomas.D <thomlgt@hotmail.fr>
2021-11-25 13:40:35 +0100

    admin model update

--- back/src/models/admin.ts
+++ back/src/models/admin.ts
@@ -1,5 +1,17 @@
-export class Admin {
-    id : string;
-    username : string;
-    password : string;
+import mongoose from "mongoose";
+
+export class AdminDocument extends mongoose.Document {
+    username : String;
+    password : String;
 }
+
+const adminSchema = new mongoose.Schema({
+    username : {
+        type: String
+    },
+    password : {
+        type: String
+    }
+})
+
+export const Admin = mongoose.model<AdminDocument>("Admin", adminSchema);
