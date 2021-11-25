794c687be1607cbc9d73a4e1158ba4f15ab67ac5
Thomas.D <thomlgt@hotmail.fr>
2021-11-25 13:40:09 +0100

    captain model update

--- back/src/models/captain.ts
+++ back/src/models/captain.ts
@@ -1,10 +1,37 @@
-export class Captain {
-    id : string;
-    firstname : string;
-    lastname : string;
-    email : string;
-    password : string;
-    phone : string;
+import mongoose from 'mongoose';
+
+class CaptainDocument extends mongoose.Document {
+    firstname : String;
+    lastname : String;
+    email : String;
+    password : String;
+    phone : String;
     birthdate : Date;
-    gender : boolean;
+    gender : Boolean;
 }
+
+const captainSchema = new mongoose.Schema({
+    firstname : {
+        type : String
+    },
+    lastname : {
+        type : String
+    },
+    email : {
+        type : String
+    },
+    password : {
+        type : String
+    },
+    phone : {
+        type : String
+    },
+    birthdate : {
+        type : Date
+    },
+    gender : {
+        type : Boolean
+    }
+});
+
+export const Captain = mongoose.model<CaptainDocument>("Captain", captainSchema);
