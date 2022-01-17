7c6a2b5f26e4ec6cd777f84f7a730c73c77efe58
Thomas.D <thomlgt@hotmail.fr>
2022-01-17 13:43:52 +0100

    add - [app.module.ts] connexion mongodb

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/ms-db')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
