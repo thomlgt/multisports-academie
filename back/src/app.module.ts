07da702334d26784baaf6cd171fa870a4283fd54
Thomas.D <thomlgt@hotmail.fr>
2022-01-19 17:01:00 +0100

    fix - [modules] déplacement capitaine dans modules

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CaptainModule } from './captain/captain.module';
+import { CaptainModule } from './modules/captain/captain.module';
 
 @Module({
   imports: [
