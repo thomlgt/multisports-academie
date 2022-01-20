afd3baeba86179fa017c14affa71cac53f3873e9
Thomas.D <thomlgt@hotmail.fr>
2022-01-20 12:18:28 +0100

    add - [team] ajout membre, suppression membre, modification nom equipe

--- back/src/modules/team/models/member.ts
+++ back/src/modules/team/models/member.ts
@@ -1,6 +1,12 @@
+import { IsNotEmpty } from "class-validator";
+
 export class Member {
+    @IsNotEmpty()
     firstname: string;
+    @IsNotEmpty()
     lastname: string;
+    @IsNotEmpty()
     gender: number;
+    @IsNotEmpty()
     birthdate: Date;
 }
--- back/src/modules/team/team.service.ts
+++ back/src/modules/team/team.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Member } from './models/member';
 import { Team, TeamDocument } from './models/team.schema';
 
 @Injectable()
@@ -17,6 +18,35 @@ export class TeamService {
 
     async findAll() {
         return this.teamModel.find().populate('captain');
+    }
+
+    async findById(id: string) {
+        return this.teamModel.findById(id).populate('captain');
+    }
+
+    async delete(id: string) {
+        return this.teamModel.findByIdAndDelete(id);
+    }
+
+    async addMember(id: string, member: Member) {
+        return this.teamModel.findByIdAndUpdate(
+            id, 
+            {$push : {members: member}, updatedDate: new Date()},
+            {new: true});
+    }
+
+    async deleteMember(id: string, member: Member) {
+        return this.teamModel.findByIdAndUpdate(
+            id, 
+            {$pull: {members: member}, updatedDate: new Date()},
+            {new: true});
+    }
+
+    async updateName(id: string, name: string) {
+        return this.teamModel.findByIdAndUpdate(
+            id, 
+            {name: name, updatedDate: new Date()},
+            {new: true});
     }
 
 
