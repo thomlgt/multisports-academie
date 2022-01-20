e57e51ff75b78fe888424d7868dbde00ed1d693d
Christine BRASSART <chrisbras89@gmail.com>
2022-01-20 17:32:43 +0100

    add - [picture] ajout du fichier pictureModule + import dans le app.module

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -6,21 +6,21 @@ import { CaptainModule } from './modules/captain/captain.module';
 import { TeamModule } from './modules/team/team.module';
 import { EventModule } from './modules/event/event.module';
 import LogsMiddleware from './middlewares/logsMiddleware';
+import { PictureModule } from './modules/picture/picture.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/ms-db'),
     CaptainModule,
     TeamModule,
-    EventModule
+    EventModule,
+    PictureModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(LogsMiddleware)
-      .forRoutes('*');
+    consumer.apply(LogsMiddleware).forRoutes('*');
   }
 }
---
+++ back/src/modules/picture/picture.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { PictureService } from './picture.service';
+import { PictureController } from './picture.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Picture, PictureSchema } from './models/picture.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Picture.name, schema: PictureSchema }]),
+  ],
+  controllers: [PictureController],
+  providers: [PictureService],
+})
+export class PictureModule {}
