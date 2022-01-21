a2403568b7cb4c9344fbb652ad3e102314bcbbdc
Samuel Boussus <samuel@toutenpixel.fr>
2022-01-21 17:44:35 +0100

    fix - [event] dtos

--- back/src/modules/event/dtos/createEvent.ts
+++ back/src/modules/event/dtos/createEvent.ts
@@ -1,4 +1,6 @@
+import { Prop } from "@nestjs/mongoose";
 import { IsDateString, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { SchemaTypes } from "mongoose";
 import { Picture } from "src/modules/picture/models/picture.schema";
 import { Registration } from "../models/registration";
 
@@ -24,6 +26,7 @@ export class CreateEvent {
     price: number;    
     @IsNumber()
     maxTeams: number;
+    @Prop({type: SchemaTypes.ObjectId, ref: Picture.name})
     picture: Picture;    
     gallery: Picture[];    
     activities: string[];    
