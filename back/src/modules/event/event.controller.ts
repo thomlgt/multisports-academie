5b088192e6bba4e14de9ec2809c570782b21f7a0
Christine BRASSART <chrisbras89@gmail.com>
2022-01-23 19:38:18 +0100

    add - [swagger] ajout des commentaires sur les méthodes des différents controller

--- back/src/modules/event/event.controller.ts
+++ back/src/modules/event/event.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 import { CreateEvent } from './dto/create-event.dto';
 import { EventService } from './event.service';
 import { Event } from './entities/event.entity';
@@ -10,26 +10,55 @@ export class EventController {
 
     constructor(private eventService: EventService) { }
 
+    /**
+     * Crée un nouvel événement
+     * @param event 
+     * @returns 
+     */
+    @ApiBody({
+        type: CreateEvent,
+        description: "l'objet Event à creer",
+    })
     @Post()
     async create(@Body() event: CreateEvent) {
         return this.eventService.create(event);
     }
 
+    /**
+     * 
+     * @returns Retourne tous les événements en base
+     */
     @Get()
     async findAll() {
         return this.eventService.findAll();
     }
 
+    /**
+     * tourne un événement à partir de son id
+     * @param id 
+     * @returns 
+     */
     @Get(':id')
     async findById(@Param('id') id : string) {
         return this.eventService.findById(id);
     }
 
+    /**
+     * supprime un événement à partir de son id
+     * @param id 
+     * @returns 
+     */
     @Delete(':id')
     async delete(@Param('id') id : string) {
         return this.eventService.delete(id);
     }
 
+    /**
+     * Modifie un événement
+     * @param id 
+     * @param event 
+     * @returns 
+     */
     @Patch(':id')
     async update(@Param('id') id : string, @Body() event : Event) {
         return this.eventService.update(id, event);
