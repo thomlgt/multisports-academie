33f89f3ae32c04580db80e785d3557c00111350d
Thomas.D <thomlgt@hotmail.fr>
2022-02-16 17:02:11 +0100

    add - [profile] ajout page profil

--- front/src/app/modules/client/client.module.ts
+++ front/src/app/modules/client/client.module.ts
@@ -5,11 +5,13 @@ import { ClientRoutingModule } from './client-routing.module';
 import { ClientComponent } from './client.component';
 import { MsApiModule } from '../ms-api/ms-api.module';
 import { MsUiModule } from '../ms-ui/ms-ui.module';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 
 @NgModule({
   declarations: [
-    ClientComponent
+    ClientComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule,
