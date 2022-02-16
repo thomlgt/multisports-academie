c31353657506ebec659fe313b9aa38eeead9d3b7
Thomas.D <thomlgt@hotmail.fr>
2022-02-16 10:49:30 +0100

    fix - [article] fix bug article module

--- back/src/modules/articles/article.module.ts
+++ back/src/modules/articles/article.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { ArticleController } from './article.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Article, ArticleSchema } from './entities/article.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+        { name : Article.name, schema : ArticleSchema }
+      ])
+    ],
   controllers: [ArticleController],
+  exports: [ArticleService],
   providers: [ArticleService]
 })
 export class ArticleModule {}
