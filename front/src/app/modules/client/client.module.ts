f0ebbadd06f5e1a231c9f16d617d85cda04d8f36
Samuel Boussus <samuel@toutenpixel.fr>
2022-02-17 16:15:06 +0100

    fix - déplacement composant events-reading-event vers pages vers composant

--- front/src/app/modules/client/client.module.ts
+++ front/src/app/modules/client/client.module.ts
@@ -8,11 +8,11 @@ import { MsUiModule } from '../ms-ui/ms-ui.module';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { TeamsEditComponent } from './pages/teams-edit/teams-edit.component';
 import { EventsReadingComponent } from './pages/events-reading/events-reading.component';
-import { EventsReadingEventComponent } from './pages/events-reading/events-reading-event/events-reading-event.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { UpdatePersonalFormComponent } from './components/profile/update-personal-form/update-personal-form.component';
 import { UpdatePasswordFormComponent } from './components/profile/update-password-form/update-password-form.component';
+import { EventsReadingEventComponent } from './components/events/events-reading-event/events-reading-event.component';
 
 
 @NgModule({
