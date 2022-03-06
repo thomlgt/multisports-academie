9af7d85d37929bfd60533bc791617218d12870dd
Samuel Boussus <samuel@toutenpixel.fr>
2022-03-06 17:59:30 +0100

    feature - [event] dynmisation

--- front/src/app/modules/client/pages/event/event.component.ts
+++ front/src/app/modules/client/pages/event/event.component.ts
@@ -11,18 +11,35 @@ import { EventService } from 'src/app/modules/ms-api/event/event.service';
 export class EventComponent implements OnInit {
 
   event: Event;
+  eventRegistrationStatus: number;
 
   constructor(private eventService: EventService) { }
 
   ngOnInit(): void {
-    this.initEvent();
+    this.initEvent();    
   }
 
   initEvent() {
     let tempId = '620ad41ccf00afacd6b8f140';
     this.eventService.findById(tempId).subscribe(data => {
       this.event = data;
-    })
+      this.defineEventRegistrationStatus();
+    })    
+  }
+
+  defineEventRegistrationStatus() {
+
+    let currentDate = new Date();
+    let startRegistration = new Date(this.event.startRegistration);
+    let endRegistration = new Date(this.event.endRegistration);
+
+    if (currentDate < startRegistration) {
+      this.eventRegistrationStatus = 1;
+    } else if (currentDate >= startRegistration && currentDate <= endRegistration) {
+      this.eventRegistrationStatus = 2;
+    } else {
+      this.eventRegistrationStatus = 3;
+    }
   }
 
 }
