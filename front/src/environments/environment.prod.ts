9279b8a481d087d278dc04d737da96a0715f949b
Thomas.D <thomlgt@hotmail.fr>
2022-03-15 17:37:13 +0100

    fix - [prox_pass]

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env.NG_APP_API_URL || "https://multisports-academie.fr:3000/api"
+  apiUrl: process.env.NG_APP_API_URL || "https://multisports-academie.fr/api"
 };
