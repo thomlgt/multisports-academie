bc5c306ef2da071530aa8d6d5dc565d5f5e63dd2
Thomas.D <thomlgt@hotmail.fr>
2022-03-15 10:58:50 +0000

    fix - [docker] environment et bdd connexion

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env.API_URL || "http://localhost:3000"
+  apiUrl: process.env.NG_APP_API_URL || "http://multisports-academie.fr:3000"
 };
