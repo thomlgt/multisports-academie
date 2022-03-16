5cfbb89e0719b69850e23063922ab477dae6fc52
Samuel Boussus <samuel@toutenpixel.fr>
2022-03-16 17:23:56 +0100

    feature - [activity] dynamisation

--- front/src/app/modules/client/pages/activity/activity.component.ts
+++ front/src/app/modules/client/pages/activity/activity.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Activity } from 'src/app/models/activity/activity.model';
+import { ActivityService } from 'src/app/modules/ms-api/activity/activity.service';
 
 @Component({
   selector: 'app-activity',
@@ -7,12 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityComponent implements OnInit {
 
+  id: string;
+  activity: Activity;
   show: string;
 
-  constructor() { }
+  constructor(
+    private activityService: ActivityService,
+    private route : ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.initActivity();
     this.show = "description";
+  }
+
+  initActivity() {
+    this.activityService.findById(this.id).subscribe(data => {
+      this.activity = data;
+      console.log(data);
+    })    
   }
 
 }
