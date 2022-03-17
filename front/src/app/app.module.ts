08578c67748f591327a4cb3c518c3efb34edd3a9
Khatastrov <chrisbras89@gmail.com>
2022-03-17 11:05:23 +0100

    add [article] ajout d'un pipe pour tronquer 'ellipsis' les extraits d'article

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,6 +6,10 @@ import { AppComponent } from './app.component';
 import { MsApiModule } from './modules/ms-api/ms-api.module';
 import { MsUiModule } from './modules/ms-ui/ms-ui.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [
@@ -18,7 +22,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     MsApiModule,
     NgbModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: "fr-FR" }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
