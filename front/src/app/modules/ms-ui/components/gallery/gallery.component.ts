8acf6ca7a775f9a345267b117116b390e3d00cb2
Khatastrov <chrisbras89@gmail.com>
2022-03-18 10:27:39 +0100

    add [gallery] - MEP du composant gallery + ajout de la modale de visionnage photo

--- front/src/app/modules/ms-ui/components/gallery/gallery.component.ts
+++ front/src/app/modules/ms-ui/components/gallery/gallery.component.ts
@@ -1,4 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Picture } from 'src/app/models/picture/picture.model';
+
+@Component({
+  selector: 'picture-modale',
+  template: `
+    <div class="modal-body">
+    <button type="button" class="btn-close" aria-label="Close" (click)="activeModal.dismiss('Cross click')"></button>
+      <img [src]="pictures[index].url"
+         [alt]="pictures[index].altText">
+    </div>
+  `,
+  styleUrls: ['./picture-modale.scss']
+})
+export class PictureModale {
+  @Input() pictures: Picture[];
+  @Input() index: number;
+
+  constructor(public activeModal: NgbActiveModal) { }
+}
+
+
 
 @Component({
   selector: 'app-gallery',
@@ -7,9 +29,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  @Input() pictures: Picture[];
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
 
+  openModal(index: number) {
+    const modalRef = this.modalService.open(PictureModale, { centered: true, size: 'xl'});
+    modalRef.componentInstance.index = index;
+    modalRef.componentInstance.pictures = this.pictures;
+  }
 }
