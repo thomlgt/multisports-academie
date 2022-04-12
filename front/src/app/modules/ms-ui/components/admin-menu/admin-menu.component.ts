87cecd19e8089858c7a8e5e787c9186767de8b3e
Thomas.D <thomlgt@hotmail.fr>
2022-04-12 14:16:15 +0200

    add - [admin] admin menu ok

--- front/src/app/modules/ms-ui/components/admin-menu/admin-menu.component.ts
+++ front/src/app/modules/ms-ui/components/admin-menu/admin-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterState } from '@angular/router';
 
 @Component({
   selector: 'ms-admin-menu',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminMenuComponent implements OnInit {
 
-  constructor() { }
+  elements = [
+    { link: '/admin/board', name: 'Tableau de bord', isActive: false },
+    { link: '/admin/captains', name: 'Capitaines', isActive: false },
+    { link: '/admin/images', name: 'Images', isActive: false },
+    { link: '/admin/activities', name: 'Activités', isActive: false },
+    { link: '/admin/events', name: 'Évènement', isActive: false },
+    { link: '/admin/articles', name: 'Actualités', isActive: false },
+  ]
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+    ) { 
+
+  }
 
   ngOnInit(): void {
+    this.initActives();
+  }
+
+  goTo(element) {
+    this.router.navigateByUrl(element.link)
+    this.resetActives();
+    this.elements.filter(elem => elem.name === element.name)[0].isActive = true;
+  }
+
+  initActives() {
+    this.elements.forEach(element => {
+      if(element.link === this.router.url) {
+        element.isActive = true;
+      }
+    })
+  }
+
+  resetActives() {
+    this.elements.forEach(element => {
+      element.isActive = false;
+    })
   }
 
 }
