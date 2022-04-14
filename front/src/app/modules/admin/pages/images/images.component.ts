4bc2ae7beee8620d9734822fc10cd9a9e3172584
Christine BRASSART <chrisbras89@gmail.com>
2022-04-14 09:43:14 +0200

    add [upload admin] - V1 de l'interface galerie admin WIP

--- front/src/app/modules/admin/pages/images/images.component.ts
+++ front/src/app/modules/admin/pages/images/images.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PictureService } from 'src/app/modules/ms-api/picture/picture.service';
 
 @Component({
   selector: 'app-images',
@@ -7,9 +8,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImagesComponent implements OnInit {
 
-  constructor() { }
+  files : any[];
+  pictures : any[];
+  loading = true;
+
+  constructor(private pictureService: PictureService) { }
 
   ngOnInit(): void {
+    this.pictureService.findAll().subscribe(
+      res => {
+        this.pictures = res;
+        this.loading = false;
+      },
+      err => {
+        console.warn(err);
+        this.loading = false;
+      }
+    )
   }
 
+  loadFiles(e) {
+    this.files = e.target.files;
+    console.log(this.files);
+  }
+
+  sendFiles() {
+    if (!this.files || this.files.length <= 0) {
+      //TODO: notif admin pas de fichiers, abort mission
+      console.log("pas de fichiers");
+      return;
+    }
+
+    const formData = new FormData();
+    Array.from(this.files).forEach(file => {
+      formData.append('images', file);
+    })
+   
+    this.pictureService.upload(formData).subscribe(res => {
+      console.log(res);
+      this.ngOnInit();
+    },
+    err => {
+      console.warn(err);
+    });
+
+  }
 }
